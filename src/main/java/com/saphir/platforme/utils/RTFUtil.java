f1bbb51f904d585cb05c20913861a9c4d2925fe8
Mootaz.benabdallah <mootezbenabdallah96@gmail.com>
2023-07-19 04:15:56 +0200

    paramtrage

---
+++ src/main/java/com/saphir/platforme/utils/RTFUtil.java
@@ -0,0 +1,32 @@
+package com.saphir.platforme.utils;
+
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.rtf.RTFEditorKit;
+
+public class RTFUtil {
+	public static String ReadRTFFile(String Filepath) throws BadLocationException, IOException {
+		String plainText = null;
+		FileInputStream stream = new FileInputStream(Filepath);
+		RTFEditorKit kit = new RTFEditorKit();
+		Document doc = kit.createDefaultDocument();
+		try {
+			kit.read(stream, doc, 0);
+
+			plainText = doc.getText(0, doc.getLength());
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		stream.close();
+		return plainText;
+		
+	}
+}
