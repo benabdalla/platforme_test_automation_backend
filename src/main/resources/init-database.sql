b8f7e80f78f92e738a86fa34ad11dbb89532b2be
Mootaz.benabdallah <mootezbenabdallah96@gmail.com>
2023-08-15 21:04:00 +0200

    action simplifier

--- src/main/resources/init-database.sql
+++ src/main/resources/init-database.sql
@@ -2,8 +2,6 @@ update users set role='ADMIN'
 
 
 insert  into utilisateur values('M1','MOOTEZ F1','M1')
-
 insert  into utilisateur values('M2','MOOTEZ F2','M2')
-
 insert  into utilisateur values('MO','MOOTEZ GROUP','MO')
 insert into parametrage values('CHROME','http://10.66.245.30/w240/','FR');
